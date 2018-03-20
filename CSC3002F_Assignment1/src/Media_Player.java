78a65c6fd1fbd68907178b6a909a5acef3d17178
Sibusiso Mayalo <mylsbu001@myuct.ac.za>
2018-03-20 19:13:01 +0200

    Fixed NotSerilizable exception

--- CSC3002F_Assignment1/src/Media_Player.java
+++ CSC3002F_Assignment1/src/Media_Player.java
@@ -11,25 +11,26 @@
 
 import java.io.*;
 import sun.audio.*;
-//import java.util.Scanner;
+import java.io.Serializable;
+import java.util.Scanner;
 
 @SuppressWarnings("restriction")
-public class Media_Player
+public class Media_Player implements Serializable
 {
-    //private String filePath;
-    private InputStream get_input_file;
+    private String filePath;
+    //private InputStream get_input_file;
     //private Scanner input_scanner;
 
    public Media_Player(String filePath) throws IOException
    {
-     get_input_file= new FileInputStream(filePath);
+     this.filePath = filePath;
    }
 
    public void play_audio() throws IOException
    {
      try
      {
-       AudioPlayer.player.start(new AudioStream(get_input_file));
+       AudioPlayer.player.start(new AudioStream(new FileInputStream(filePath)));
 
       }catch (IOException e)
       {
@@ -40,7 +41,8 @@ public class Media_Player
    public static void main(String[] args) {
      try
      {
-       Media_Player m = new Media_Player("");
+       Scanner input = new Scanner(System.in);
+       Media_Player m = new Media_Player(input.nextLine());
        m.play_audio();
      }catch(IOException e){}
 
