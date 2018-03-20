80b96fde9b8f4b23cc982180c5c62bc46335431c
Yamkela Venfolo <yamkela.venfolo@SAMAC137.local>
2018-03-20 09:29:13 +0200

    master branch updated

--- CSC3002F_Assignment1/src/ClientImageDisplayer.java
+++ CSC3002F_Assignment1/src/ClientImageDisplayer.java
@@ -4,6 +4,7 @@ import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+
 /***
  * A class to display Image Messages sent to the Client.
  */
